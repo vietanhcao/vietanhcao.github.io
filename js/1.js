770438cd953ccbf8f323c7f294d6df81c1faa3f7
caoviet <vietanhcao1994@gmail.com>
2018-09-14 22:52:38 +0700

    add login and bannerduoi

--- js/1.js
+++ js/1.js
@@ -1,16 +1,12 @@
 $(function() { 
     var offset1 = $('.andi1').offset();
-        var top1 = offset1.top - 600;
+    var top1 = offset1.top - 600;
     var offset2 = $('.andi2').offset();
-        var top2 = offset2.top - 700;
+    var top2 = offset2.top - 700;
     var offset3 = $('.andi3').offset();
-        var top3 = offset3.top - 700;
+    var top3 = offset3.top - 700;
     var offset4 = $('.andi4').offset();
-        var top4 = offset4.top - 700;
-
-    
-
-
+    var top4 = offset4.top - 700;
     $(window).scroll(function () { 
         
         if ($('body,html').scrollTop() > 50){
@@ -30,15 +26,19 @@ $(function() {
         }
         if ($('body,html').scrollTop() >= top4){
             $('.andi4').addClass('hienra');
+        }  
+        //hieu ung di len
+        if($('html, body').scrollTop() > 300){
+            $('.nutscroll button').addClass('hienthi');
+        }else{
+            $('.nutscroll button').removeClass('hienthi');
         }
-       
-
         
-       
-
     });
 });
-
+$('.nutscroll button').click(function () { 
+    $('body,html').animate({scrollTop : 0},1000); 
+});
 // $(function() {
 // 	$(window).scroll(function() {
 // 		if( $('html, body').scrollTop()>500) {
