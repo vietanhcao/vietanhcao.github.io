720188bd25c71909cca240b3865e436cd1d88952
caoviet <vietanhcao1994@gmail.com>
2018-09-18 23:40:49 +0700

    edit and varidate sign in

--- js/1.js
+++ js/1.js
@@ -1,4 +1,5 @@
 $(function() { 
+    "use strict";
     var offset1 = $('.andi1').offset();
     var top1 = offset1.top - 600;
     var offset2 = $('.andi2').offset();
@@ -35,20 +36,104 @@ $(function() {
         }
         
     });
+    $('.nutscroll button').click(function () { 
+        $('body,html').animate({scrollTop : 0},1000); 
+    });
+    // validate form
+    
+    
+    let emailRgx = /^([a-zA-Z0-9]{3,20})+@+([a-z-]{3,10})\.([a-z]{3,5})$/;
+    let namesrex = /^([a-zA-Z-0-9]{3,20})$/;
+    let passworkrex = /^([a-zA-Z-0-9]{6,99})$/;
+    $('#namesignup').change(function () { 
+        if (!namesrex.test($('#namesignup').val())){
+            $('#fullname-error').css('opacity',1);
+            $('#fullname-error').css('visibility','visible');
+            $('#checkfullname').css('opacity', 0);
+            
+        }else{
+            $('#fullname-error').css('opacity', 0);
+            $('#fullname-error').css('visibility', 'hiden');
+            $('#checkfullname').css('opacity',1);
+        } 
+    });
+    $('#username').change(function () { 
+        if (!namesrex.test($('#username').val())){
+            $('.username .help-block').css('opacity',1);
+            $('.username .help-block').css('visibility','visible');
+            $('.username .fas.fa-check').css('opacity', 0);
+            
+        }else{
+            $('.username .help-block').css('opacity', 0);
+            $('.username .help-block').css('visibility', 'hiden');
+            $('.username .fas.fa-check').css('opacity',1);
+        } 
+    });
+    $('#emailsignup').change(function () { 
+        if (!emailRgx.test($('#emailsignup').val())){
+            $('.email-signup .help-block').css('opacity',1);
+            $('.email-signup .help-block').css('visibility','visible');
+            $('.email-signup .fas.fa-check').css('opacity', 0);
+            
+        }else{
+            $('.email-signup .help-block').css('opacity', 0);
+            $('.email-signup .help-block').css('visibility', 'hiden');
+            $('.email-signup .fas.fa-check').css('opacity',1);
+        } 
+    });
+    $('#password').change(function () {
+        if (!passworkrex.test($('#password').val())) {
+            $('.password .help-block').css('opacity', 1);
+            $('.password .help-block').css('visibility', 'visible');
+            $('.password .fas.fa-check').css('opacity', 0);
+            
+        } else {
+            $('.password .help-block').css('opacity', 0);
+            $('.password .help-block').css('visibility', 'hiden');
+            $('.password .fas.fa-check').css('opacity', 1);
+        }
+    });
+    
+    $('#confirm').change(function () {
+        if (!passworkrex.test($('#password').val())){
+            
+        }else if ($('#password').val() != $('#confirm').val()) {
+            $('.confirm .help-block').css('opacity', 1);
+            $('.confirm .help-block').css('visibility', 'visible');
+            $('.confirm .fas.fa-check').css('opacity', 0);
+
+        } else {
+            $('.confirm .help-block').css('opacity', 0);
+            $('.confirm .help-block').css('visibility', 'hiden');
+            $('.confirm .fas.fa-check').css('opacity', 1);
+            
+        }
+    });
+    $('#submitsingup').attr('disabled', 'true');
+    $('#checkbox').click(function () {  
+        if ($('#checkbox').prop('checked') == false) {
+            $('#submitsingup').attr('disabled','true');
+        } else if ($('#checkbox').prop('checked') == true){
+            $('#submitsingup').removeAttr('disabled');
+            
+        }
+    });
+    $('#submitsingup').click(function () { 
+        
+        if ($('#password').val() != $('#confirm').val()) {
+            return false;
+        } 
+    });
+
+    
+    
+    
+    
+    
+    
+    
 });
-$('.nutscroll button').click(function () { 
-    $('body,html').animate({scrollTop : 0},1000); 
-});
-// $(function() {
-// 	$(window).scroll(function() {
-// 		if( $('html, body').scrollTop()>500) {
-// 			$('.nutlen').addClass('hienthi');
-// 		}else {
-// 			$('.nutlen').removeClass('hienthi');
-// 		}
-// 	});
-// 	$('.nutlen').click(function() {
-// 		$('html, body').animate({'scrollTop':0} );
-// 		// return false; //khi click vào thẻ a nó sẽ load lại web false để ko load nữa
-// 	});
-// })
+
+
+
+
